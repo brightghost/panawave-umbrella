f3bc6f78a69dc19aaa5de99d2d29719226570e4b
brightghost <walkersam@gmail.com>
2016-07-03 13:26:01 -0500

    Added profiling function.

--- panawave-umbrella.py
+++ panawave-umbrella.py
@@ -16,8 +16,23 @@ def pw_json_serializer(object):
     except (NameError, AttributeError):
         return object.__dict__
 
+def tracefunc(frame, event, arg, indent=[0]):
+    '''Debugging function which can be attached to all function calls.
+    http://stackoverflow.com/questions/8315389/how-do-i-print-functions-as-they-are-called
+    '''
+    if event == "call":
+        indent[0] += 2
+        print("-" * indent[0] + "> call function", frame.f_code.co_name)
+    elif event == "return":
+        print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+        indent[0] -= 2
+    return tracefunc
+
 if __name__ == "__main__":
     print("initializing Panawave Umbrella Editor...")
+    profile == False
+    if profile == True:
+        sys.setprofile(tracefunc)
     master = Tk()
     # global our_app
     our_app = PanawaveApp(master)
