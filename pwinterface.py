9fc5615f41aabf790d7c50bc77636b970a9e0202
brightghost <walkersam@gmail.com>
2016-07-05 00:53:31 -0500

    Removing deprecated functions.

--- pwinterface.py
+++ pwinterface.py
@@ -15,20 +15,6 @@ from IPython import embed
 from radialstructs import *
 from pwwidgets import *
 
-def draw_canvas(tkinstance):
-    # basic assumptions of the canvas: origin at center, radial
-    # positions specified by setting radius along pos. Y axis and
-    # then performing clockwise rotation.
-
-    w = Canvas(tkinstance, width=800, height=800)
-    # this will position origin at center
-    w.configure(scrollregion=(-400,-400,400,400))
-    w.pack()
-    # center crosshairs
-    w.create_line(-40, -20, 40, 20, fill="red", dash=(4, 4))
-    w.create_line(-40, 20, 40, -20, fill="red", dash=(4, 4))
-    return w
-
 
 class PanawaveApp:
     '''our GUI app for working with PanawaveStructs.
@@ -227,146 +213,146 @@ class PanawaveApp:
 
 # ===========================================================================
 # Reference shite left over below.
-    def _update_ring_selection(self, event):
-        '''Bound to click events on listbox. pw_list_box.selection()
-        returns an IID; we explicitly set these when refreshing the list
-        to make it easier to do this lookup.'''
-        list_selection_array = self.pw_list_box.selection()
-        print("List box is reporting current selection as: ",
-                list_selection_array)
-        self.pw_interface_selected_rings = []
-        # there's probably a more clever way to do this with a list
-        # comprehension but the scoping issues with listcomps are way over
-        # my head at the moment:
-        # http://stackoverflow.com/questions/13905741/accessing-class-variables-from-a-list-comprehension-in-the-class-definition
-        # Just going to reset the .selected atribute on all rings then
-        # set it again based on what's reported by the TreeView.
-        for ring in self.working_struct.ring_array.values():
-            ring.selected = False
-        for iid in list_selection_array:
-            selected_ring = self.working_struct.ring_array[iid]
-            self.pw_interface_selected_rings.append(selected_ring)
-            selected_ring.selected = True
-        self.pw_canvas.delete("all")
-        self.working_struct.draw(self.pw_canvas)
-
-    # Sliders modify selected ring's attributes in realtime;
-    # if no ring is selected they just adjust the input value
-    # and wait for the 'Submit' button to do anything with them.
-
-    def update_active_ring_radius(self, rad):
-        print("updating ring radius with value: ", rad)
-        if len(self.pw_interface_selected_rings) == 1:
-            self.pw_interface_selected_rings[0].set_radius(rad)
-        self.pw_input_radius.delete(0, END)
-        self.pw_input_radius.insert(0, rad)
-
-    def update_active_ring_count(self, count):
-        if self.selected_ring is not None:
-            self.selected_ring.set_count(int(count))
-            self.selected_ring.draw(self.pw_canvas)
-        self.pw_input_count.delete(0, END)
-        self.pw_input_count.insert(0, count)
-
-    def update_active_ring_offset(self, deg):
-        if self.selected_ring is not None:
-            self.selected_ring.set_offset(deg)
-            self.selected_ring.draw(self.pw_canvas)
-        self.pw_input_offset.delete(0, END)
-        self.pw_input_offset.insert(0, deg)
-
-
-    def load_new_struct(self, file=None, target_canvas=None):
-        '''create an empty struct, attach it to the canvas,
-        and populate it from a file if one is given.'''
-        self.selected_ring = None
-        self.working_struct = PanawaveStruct(canvas=target_canvas)
-        if file is not None:
-            self.working_struct.load_from_file(file)
-        self.working_struct.draw()
-        self.update_list_box()
-        return self.working_struct
-
-    def submit_new_ring(self, *args):
-        '''validate the input and submit it to our current struct.
-        will accept event objects from bindings but currently ignores
-        them.'''
-        self.working_struct.add_ring(self.pw_input_radius.get(), \
-                self.pw_input_count.get(), \
-                self.pw_input_offset.get())
-        self.working_struct.draw(self.pw_canvas)
-        self.pw_input_radius.focus_set()
-        self.update_list_box()
-
-    def toggle_animation(self):
-        if self.working_struct.animating is True:
-            self.working_struct.stop_animation()
-            self.pw_orbit_toggle.configure(text="Start")
-        else:
-            self.working_struct.orbit_randomly()
-            self.pw_orbit_toggle.configure(text="Stop")
-
-    def orbit_randomly(self):
-        self.pw_orbit_toggle.configure(text="Stop")
-        self.working_struct.orbit(method="random")
-
-    def orbit_linearly(self):
-        self.pw_orbit_toggle.configure(text="Stop")
-        self.working_struct.orbit(method="linear")
-
-    def orbit_inverse_linearly(self):
-        self.pw_orbit_toggle.configure(text="Stop")
-        self.working_struct.orbit(method="inverse-linear")
-
-    def execute_console_input(self, *args):
-        '''execute arbitrary commands from the console box,
-        update the UI, and clear input's contents'''
-        statement = self.pw_console.get()
-        self.console_history.append(statement)
-        try:
-            eval(statement)
-        except:
-            e = sys.exc_info()
-            print("***Console input generated the following error:***")
-            print(e)
-        self.working_struct.draw(self.pw_canvas)
-        self.update_list_box()
-        sleep(.5)
-        self.console_history_offset = 0
-        self.pw_console.delete(0, END)
-
-    def navigate_console_history(self, event):
-        '''walk through input history and replace pw_console contents
-        the offset is stored as a negative integer, used directly as
-        a reverse index. This is fine because although the list length
-        changes every time we input a command, we're not caring about
-        saving the history index then anyway.'''
-        print("keypress received: ", event.keysym)
-        if event.keysym == "Up":
-            new_offset = self.console_history_offset - 1
-        elif event.keysym == "Down":
-            new_offset = self.console_history_offset + 1
-        print("testing offset: ", new_offset)
-        hist = self.console_history
-        hist_len = len(hist)
-        print("hist len: ", hist_len)
-        if new_offset >= 0:
-            # return to a blank slate if we arrive back at the
-            # end of the history.
-            self.pw_input_offset = 0
-            self.pw_console.delete(0, END)
-            print("reset offset to zero.")
-            return
-        if (0 > new_offset >= -hist_len):
-            self.console_history_offset = new_offset
-            self.pw_console.delete(0, END)
-            self.pw_console.insert(END, hist[new_offset])
-            print ("decided offset ", new_offset, " is valid.")
-
-
-if __name__ == "__main__":
-    print("initializing Panawave Umbrella Editor...")
-    master = Tk()
-    global our_app
-    our_app = PanawaveApp()
+#     def _update_ring_selection(self, event):
+#         '''Bound to click events on listbox. pw_list_box.selection()
+#         returns an IID; we explicitly set these when refreshing the list
+#         to make it easier to do this lookup.'''
+#         list_selection_array = self.pw_list_box.selection()
+#         print("List box is reporting current selection as: ",
+#                 list_selection_array)
+#         self.pw_interface_selected_rings = []
+#         # there's probably a more clever way to do this with a list
+#         # comprehension but the scoping issues with listcomps are way over
+#         # my head at the moment:
+#         # http://stackoverflow.com/questions/13905741/accessing-class-variables-from-a-list-comprehension-in-the-class-definition
+#         # Just going to reset the .selected atribute on all rings then
+#         # set it again based on what's reported by the TreeView.
+#         for ring in self.working_struct.ring_array.values():
+#             ring.selected = False
+#         for iid in list_selection_array:
+#             selected_ring = self.working_struct.ring_array[iid]
+#             self.pw_interface_selected_rings.append(selected_ring)
+#             selected_ring.selected = True
+#         self.pw_canvas.delete("all")
+#         self.working_struct.draw(self.pw_canvas)
+# 
+#     # Sliders modify selected ring's attributes in realtime;
+#     # if no ring is selected they just adjust the input value
+#     # and wait for the 'Submit' button to do anything with them.
+# 
+#     def update_active_ring_radius(self, rad):
+#         print("updating ring radius with value: ", rad)
+#         if len(self.pw_interface_selected_rings) == 1:
+#             self.pw_interface_selected_rings[0].set_radius(rad)
+#         self.pw_input_radius.delete(0, END)
+#         self.pw_input_radius.insert(0, rad)
+# 
+#     def update_active_ring_count(self, count):
+#         if self.selected_ring is not None:
+#             self.selected_ring.set_count(int(count))
+#             self.selected_ring.draw(self.pw_canvas)
+#         self.pw_input_count.delete(0, END)
+#         self.pw_input_count.insert(0, count)
+# 
+#     def update_active_ring_offset(self, deg):
+#         if self.selected_ring is not None:
+#             self.selected_ring.set_offset(deg)
+#             self.selected_ring.draw(self.pw_canvas)
+#         self.pw_input_offset.delete(0, END)
+#         self.pw_input_offset.insert(0, deg)
+# 
+# 
+#     def load_new_struct(self, file=None, target_canvas=None):
+#         '''create an empty struct, attach it to the canvas,
+#         and populate it from a file if one is given.'''
+#         self.selected_ring = None
+#         self.working_struct = PanawaveStruct(canvas=target_canvas)
+#         if file is not None:
+#             self.working_struct.load_from_file(file)
+#         self.working_struct.draw()
+#         self.update_list_box()
+#         return self.working_struct
+# 
+#     def submit_new_ring(self, *args):
+#         '''validate the input and submit it to our current struct.
+#         will accept event objects from bindings but currently ignores
+#         them.'''
+#         self.working_struct.add_ring(self.pw_input_radius.get(), \
+#                 self.pw_input_count.get(), \
+#                 self.pw_input_offset.get())
+#         self.working_struct.draw(self.pw_canvas)
+#         self.pw_input_radius.focus_set()
+#         self.update_list_box()
+# 
+#     def toggle_animation(self):
+#         if self.working_struct.animating is True:
+#             self.working_struct.stop_animation()
+#             self.pw_orbit_toggle.configure(text="Start")
+#         else:
+#             self.working_struct.orbit_randomly()
+#             self.pw_orbit_toggle.configure(text="Stop")
+# 
+#     def orbit_randomly(self):
+#         self.pw_orbit_toggle.configure(text="Stop")
+#         self.working_struct.orbit(method="random")
+# 
+#     def orbit_linearly(self):
+#         self.pw_orbit_toggle.configure(text="Stop")
+#         self.working_struct.orbit(method="linear")
+# 
+#     def orbit_inverse_linearly(self):
+#         self.pw_orbit_toggle.configure(text="Stop")
+#         self.working_struct.orbit(method="inverse-linear")
+# 
+#     def execute_console_input(self, *args):
+#         '''execute arbitrary commands from the console box,
+#         update the UI, and clear input's contents'''
+#         statement = self.pw_console.get()
+#         self.console_history.append(statement)
+#         try:
+#             eval(statement)
+#         except:
+#             e = sys.exc_info()
+#             print("***Console input generated the following error:***")
+#             print(e)
+#         self.working_struct.draw(self.pw_canvas)
+#         self.update_list_box()
+#         sleep(.5)
+#         self.console_history_offset = 0
+#         self.pw_console.delete(0, END)
+# 
+#     def navigate_console_history(self, event):
+#         '''walk through input history and replace pw_console contents
+#         the offset is stored as a negative integer, used directly as
+#         a reverse index. This is fine because although the list length
+#         changes every time we input a command, we're not caring about
+#         saving the history index then anyway.'''
+#         print("keypress received: ", event.keysym)
+#         if event.keysym == "Up":
+#             new_offset = self.console_history_offset - 1
+#         elif event.keysym == "Down":
+#             new_offset = self.console_history_offset + 1
+#         print("testing offset: ", new_offset)
+#         hist = self.console_history
+#         hist_len = len(hist)
+#         print("hist len: ", hist_len)
+#         if new_offset >= 0:
+#             # return to a blank slate if we arrive back at the
+#             # end of the history.
+#             self.pw_input_offset = 0
+#             self.pw_console.delete(0, END)
+#             print("reset offset to zero.")
+#             return
+#         if (0 > new_offset >= -hist_len):
+#             self.console_history_offset = new_offset
+#             self.pw_console.delete(0, END)
+#             self.pw_console.insert(END, hist[new_offset])
+#             print ("decided offset ", new_offset, " is valid.")
+# 
+# 
+# if __name__ == "__main__":
+#     print("initializing Panawave Umbrella Editor...")
+#     master = Tk()
+#     global our_app
+#     our_app = PanawaveApp()
 
